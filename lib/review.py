3b6388637190e29177f801fd6746647527dcffa4
dylan-indieka <dylan.indieka@student.moringaschool.com>
2025-06-03 03:57:35 -0700

    save changes

--- lib/review.py
+++ lib/review.py
@@ -1,82 +1,32 @@
-from __init__ import CURSOR, CONN
-from department import Department
-from employee import Employee
-
+from ..config import CONN, CURSOR
 
 class Review:
-
-    # Dictionary of objects saved to the database.
     all = {}
 
-    def __init__(self, year, summary, employee_id, id=None):
+    def __init__(self, year, summary, employee, id=None):
         self.id = id
         self.year = year
         self.summary = summary
-        self.employee_id = employee_id
+        self.employee = employee
 
     def __repr__(self):
-        return (
-            f"<Review {self.id}: {self.year}, {self.summary}, "
-            + f"Employee: {self.employee_id}>"
-        )
+        return f"<Review {self.id}: {self.year}, {self.summary[:20]}...>"
 
     @classmethod
     def create_table(cls):
-        """ Create a new table to persist the attributes of Review instances """
         sql = """
-            CREATE TABLE IF NOT EXISTS reviews (
+        CREATE TABLE IF NOT EXISTS reviews (
             id INTEGER PRIMARY KEY,
-            year INT,
+            year INTEGER,
             summary TEXT,
             employee_id INTEGER,
-            FOREIGN KEY (employee_id) REFERENCES employee(id))
+            FOREIGN KEY (employee_id) REFERENCES employees(id)
+        )
         """
         CURSOR.execute(sql)
         CONN.commit()
 
     @classmethod
     def drop_table(cls):
-        """ Drop the table that persists Review  instances """
-        sql = """
-            DROP TABLE IF EXISTS reviews;
-        """
-        CURSOR.execute(sql)
+        CURSOR.execute("DROP TABLE IF EXISTS reviews")
         CONN.commit()
-
-    def save(self):
-        """ Insert a new row with the year, summary, and employee id values of the current Review object.
-        Update object id attribute using the primary key value of new row.
-        Save the object in local dictionary using table row's PK as dictionary key"""
-        pass
-
-    @classmethod
-    def create(cls, year, summary, employee_id):
-        """ Initialize a new Review instance and save the object to the database. Return the new instance. """
-        pass
-   
-    @classmethod
-    def instance_from_db(cls, row):
-        """Return an Review instance having the attribute values from the table row."""
-        # Check the dictionary for  existing instance using the row's primary key
-        pass
-   
-
-    @classmethod
-    def find_by_id(cls, id):
-        """Return a Review instance having the attribute values from the table row."""
-        pass
-
-    def update(self):
-        """Update the table row corresponding to the current Review instance."""
-        pass
-
-    def delete(self):
-        """Delete the table row corresponding to the current Review instance,
-        delete the dictionary entry, and reassign id attribute"""
-        pass
-
-    @classmethod
-    def get_all(cls):
-        """Return a list containing one Review instance per table row"""
-        pass
-
